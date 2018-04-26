45e66d7ee95c8c882502144db15433e9094203df
Daniel Lerch <daniel-lerch@live.de>
2018-04-26 23:18:51 +0200

    Created solution and project

---
+++ src/DynDnsDistributor/Controllers/UpdateController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynDnsDistributor.Controllers
+{
+    [Route("[controller]")]
+    public class UpdateController : Controller
+    {
+        // GET update?hostname=<hostname>&myip=<ipaddr>
+        [HttpGet]
+        public IActionResult Get([FromQuery(Name = "hostname")]string hostname, [FromQuery(Name = "myip")]string myip)
+        {
+            return StatusCode(501);
+        }
+    }
+}
