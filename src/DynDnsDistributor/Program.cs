8019042599892fa2d45ff25ff0468625b57f22ad
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-12-14 16:32:48 +0530

    Migrate to .NET Core 3.1

--- src/DynDnsDistributor/Program.cs
+++ src/DynDnsDistributor/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace DynDnsDistributor
 {
@@ -13,13 +13,18 @@ namespace DynDnsDistributor
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(builder =>
-                    builder.AddJsonFile("dyndnsconfig.json", optional: false, reloadOnChange: true))
-                .UseStartup<Startup>();
+                {
+                    builder.AddJsonFile("dyndnsconfig.json", optional: false, reloadOnChange: true);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- src/DynDnsDistributor/Startup.cs
+++ src/DynDnsDistributor/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace DynDnsDistributor
 {
@@ -27,21 +28,23 @@ namespace DynDnsDistributor
             services.AddOptions();
             services.Configure<DynDnsOptions>(Configuration);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRouting();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddSingleton<DnsUpdateService>();
             services.AddHostedService<IpPollingHostedService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
