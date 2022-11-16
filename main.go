8b80314d59cf9c295dbbee42d6d10a6a44ec1289
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2022-11-16 16:24:39 +0100

    Read dyndnsconfig.json at startup

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Settings struct {
+	IpRetrieveUrl     string    `json:"IpRetrieveUrl"`
+	IpPollingInterval int       `json:"IpPollingInterval"`
+	UserAgent         string    `json:"UserAgent"`
+	Accounts          []Account `json:"Accounts"`
+}
+
+type Account struct {
+	Username   string   `json:"Username"`
+	Password   string   `json:"Password"`
+	Local      bool     `json:"Local"`
+	UpdateUrls []string `json:"UpdateUrls"`
+}
 
 func basicAuth(c *gin.Context) {
 	username, password, hasAuth := c.Request.BasicAuth()
@@ -24,9 +42,35 @@ func update(c *gin.Context) {
 
 }
 
-func main() {
-	router := gin.Default()
-	router.GET("/update", basicAuth, update)
+func load_settings() (Settings, error) {
+	var settings Settings
+	file, err := os.Open("src/DynDnsDistributor/dyndnsconfig.json")
+	if err != nil {
+		return settings, err
+	} else {
+		buffer, err := ioutil.ReadAll(file)
+		if err != nil {
+			return settings, err
+		} else {
+			json.Unmarshal(buffer, &settings)
+			return settings, nil
+		}
+	}
+}
 
-	router.Run("localhost:8080")
+func main() {
+	settings, err := load_settings()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(settings.IpRetrieveUrl)
+		fmt.Println(settings.IpPollingInterval)
+		for _, account := range settings.Accounts {
+			fmt.Println(account)
+		}
+		router := gin.Default()
+		router.GET("/update", basicAuth, update)
+
+		router.Run("localhost:8080")
+	}
 }
